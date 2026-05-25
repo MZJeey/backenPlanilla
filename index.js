beb0611a48fd343d1b61eb6f8e9344a1eb193d14
Jeeyson <jasonezezel@gmail.com>
2026-05-24 18:24:34 -0600

    Backend

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-process.on("uncaughtException", (err) => {
-  console.error("ERROR FATAL:", err.message, err.stack);
-});
-
-process.on("unhandledRejection", (reason) => {
-  console.error("PROMESA RECHAZADA:", reason);
-});
-
-require("dotenv").config();
+require("dotenv").config(); // primera línea
 const express = require("express");
 const cors = require("cors");
 const asignarRutasAExpress = require("./rutas/rutas.js");
 
 const app = express();
-
-const corsOptions = {
-  origin: "https://app-planilla.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.options("*", cors(corsOptions));
-app.use(cors(corsOptions));
-
 app.use(express.json());
+app.use(cors());
 asignarRutasAExpress(app);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Backend corriendo en el puerto ${PORT}`);
+const servidor = app.listen(80, () => {
+  console.log("Backend corriendo en el puerto 80.");
 });
